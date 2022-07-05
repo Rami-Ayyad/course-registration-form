0faf7914ea131f7fa11c93536f1fa4fb94b4293f
Rami-Ayyad <engramiahmad97@gmail.com>
2022-07-05 23:53:22 +0200

    FB

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB3KNspimVgirJM1Y6kQHxctXjJ6VzwTlU",
+    authDomain: "courses-form-3e0e2.firebaseapp.com",
+    projectId: "courses-form-3e0e2",
+    storageBucket: "courses-form-3e0e2.appspot.com",
+    messagingSenderId: "404807497812",
+    appId: "1:404807497812:web:1b40fd40908161cb9015c1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
